0af755cd12f464408149758ea128129a1fbd1b4b
Md. Ashikur Rahman <38959435+Ashik55@users.noreply.github.com>
2021-03-24 02:06:27 +0600

    Update ContentView.swift
    init

--- SwiftUI CounterApp/ContentView.swift
+++ SwiftUI CounterApp/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var counter = 0
+    
+    fileprivate func makeCountText() -> Text {
+        return Text("\(counter)")
+            .font(Font.system(size: 68))
+    }
+    
+    fileprivate func makeResetButton() -> some View {
+        return Button(action: {  self.counter = 0}, label: {
+            Text("Reset").foregroundColor(.white)
+                .bold()
+        }).frame(width: 150, height: 50, alignment: .center)
+        .overlay(RoundedRectangle(cornerRadius: 8).stroke())
+        .background(Color.red)
+        .cornerRadius(8)
+    }
+    
+    fileprivate func makeCountButton() -> some View {
+        return Button(action: {
+            self.counter += 1
+        }, label: {
+            Text("Click Me").foregroundColor(.white)
+                .bold()
+        }).frame(width: 150, height: 50, alignment: .center)
+        .overlay(RoundedRectangle(cornerRadius: 8).stroke())
+        .background(Color.blue)
+        .cornerRadius(8)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing:100){
+            
+            makeCountText()
+            
+            
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 60){
+                makeResetButton()
+                
+                makeCountButton()
+            }
+            
+      
+            
+        }
+        
+    
     }
 }
 
